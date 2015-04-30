5100ca506ad9d2a4099ccd160ac0c7ed00313d69
Shawn Toffel <shawn.toffel@gmail.com>
2015-04-29 22:58:02 -0600

    Added search method

--- lib/ruby-stackoverflow/client/search_helper.rb
+++ lib/ruby-stackoverflow/client/search_helper.rb
@@ -2,6 +2,10 @@ module RubyStackoverflow
   class Client
     module SearchHelper
 
+      def search(query, options={})
+        search_response(query, options)
+      end
+
       def similar(title, options={})
         similar_response(title, options)
       end
@@ -10,6 +14,10 @@ module RubyStackoverflow
 
       def similar_response(title, options={})
         getr 'similar', 'question', options.merge(title: title)
+      end
+
+      def search_response(query, options={})
+        getr 'search', 'question', options.merge(title: title)
       end
 
     end
